6699eb7381abe469f6631bf663ea7c4a206f4218
Tom Rose <9136302+Thomas-James-Rose@users.noreply.github.com>
2018-11-16 14:55:20 +0000

    Create test directory

--- test/Hello.test.jsx
+++ test/Hello.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Hello from './Hello.jsx';
+import Hello from '../src/Hello.jsx';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
